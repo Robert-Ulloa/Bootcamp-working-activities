81769e2b255c2d2f8826b6219aa912abb280cb70
Roberto Ulloa <robertoulloa@MacBook-Pro.attlocal.net>
2024-06-10 21:56:22 -0500

    Learning how to use Third Party APIS and dayJS

--- 05-Third-Party-APIs/01-Activities/26-Stu_DayJS-Format/Unsolved/assets/js/script.js
+++ 05-Third-Party-APIs/01-Activities/26-Stu_DayJS-Format/Unsolved/assets/js/script.js
@@ -1,16 +1,35 @@
 // Use Day.js to format the date and assign to the declared variable.
 
 // TODO: 1. What is your graduation date in the following format: Jan 1, 1999?
+const gradDate = dayjs('2023-10-15').format('MMM D, YYYY');
+$('#1a').text(gradDate);
 
 // TODO: 2. What day of the week will 1/1/2027 be? (e.g. Is it "Monday"?)
 
+const weekDay = dayjs('01-01-2027', 'M-D-YYYY').format('dddd');
+$('#2a').text(weekDay);
+
+
 // TODO: 3. What is the current time in the format: hours:minutes:seconds
+
+const time = dayjs().format('hh:mm:ss');
+$('#3a').text(time);
 
 // TODO: 4. What is the current Unix timestamp?
 
+const unix = dayjs().unix();
+$('#4a').text(unix);
+
 // TODO: 5. Parse the following Unix timestamp, 1318781876, and convert into any time/date format.
+
+const unixFormat = dayjs.unix(1318781876).format('MMM D, YYYY, hh:mm:ss');
+$('#5a').text(unixFormat);
 
 // TODO: 6. What is the difference in days between May 4, 2027 and today? Hint:
 // You can display the difference between two dayjs objects by using the dayjs
 // diff method.)
+const today = dayjs();
+const futureDate = dayjs('2027-05-04');
+const differenceInDays = futureDate.diff(today, 'day');
+$('#6a').text(differenceInDays);
 
