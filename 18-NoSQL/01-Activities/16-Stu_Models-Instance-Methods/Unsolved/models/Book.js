aa5263c3d6f0bdb401716f73e93635b8adf92d3a
RobertUlloa <roanuc8@gmail.com>
2024-09-10 21:28:17 -0500

    class stuff

--- 18-NoSQL/01-Activities/16-Stu_Models-Instance-Methods/Unsolved/models/Book.js
+++ 18-NoSQL/01-Activities/16-Stu_Models-Instance-Methods/Unsolved/models/Book.js
@@ -1,13 +1,31 @@
 const mongoose = require('mongoose');
 
-// TODO: Define a new schema named `bookSchema`
+// Define a new schema named `bookSchema`
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  price: { type: Number, required: true }
+});
 
-// TODO: Create a custom instance method named `getDiscount`
+// Create a custom instance method named `getDiscount`
+// This method reduces the price by 50% and logs the book title and discounted price
+bookSchema.methods.getDiscount = function () {
+  const discountedPrice = this.price * 0.5;
+  console.log(`The book "${this.title}" is now available for $${discountedPrice}`);
+};
 
-// TODO: Create a model named `Book`
+// Create a model named `Book`
+const Book = mongoose.model('Book', bookSchema);
 
-// TODO: Create a new instance of the model
+// Create a new instance of the model
+const discountedBook = new Book({
+  title: 'Moby Dick',
+  author: 'Herman Melville',
+  price: 20
+});
 
-// TODO: Call the custom instance method on the instance
+// Call the custom instance method on the instance
+discountedBook.getDiscount();
 
+// Export the Book model
 module.exports = Book;
